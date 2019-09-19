bfd5de555cb93513c343ce22339fc9bda7b1e381
ClarisseU <clarisseum@gmail.com>
2019-09-19 15:37:35 +0200

    add the navbar

--- app/main/views.py
+++ app/main/views.py
@@ -9,7 +9,9 @@ def index():
     view root page function that returns the index page and its data
     '''
      # Getting popular movie
+   # Getting popular movie
     popular_movies = get_movies('popular')
-    print(popular_movies)
-    title = 'Home - Welcome to the best Movie Review Website Online'
-    return render_template('index.html', message = message)
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
